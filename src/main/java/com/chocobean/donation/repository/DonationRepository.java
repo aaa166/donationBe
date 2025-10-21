ecfc9202b28ab4d4f315eb66da85d13bb8534925
DESKTOP-2PUEH4H\kmcsl <kmcslo@naver.com>
2025-10-21 22:09:44 +0900

    Repository,Service 수정(dto -> entity)

--- src/main/java/com/chocobean/donation/repository/DonationRepository.java
+++ src/main/java/com/chocobean/donation/repository/DonationRepository.java
@@ -1,41 +1,41 @@
 package com.chocobean.donation.repository;
 
-import com.chocobean.donation.dto.DonationList;
-import com.chocobean.donation.dto.DonationView;
+import com.chocobean.donation.entity.Donation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface DonationRepository extends JpaRepository<com.chocobean.donation.entity.Donation, Long> {
-    @Query("SELECT new com.chocobean.donation.dto.DonationList(d.donationTitle, d.donationOrganization, "
-            + "CAST((d.donationCurrentAmount * 100.0 / d.donationGoalAmount) AS int), "
-            + "d.donationCurrentAmount, d.donationImg, d.donationDeadlineDate) "
-            + "FROM Donation d")
-    List<DonationList> findDonationSummaries();
+//    @Query("SELECT new com.chocobean.donation.dto.DonationList(d.donationTitle, d.donationOrganization, "
+//            + "CAST((d.donationCurrentAmount * 100.0 / d.donationGoalAmount) AS int), "
+//            + "d.donationCurrentAmount, d.donationImg, d.donationDeadlineDate) "
+//            + "FROM Donation d")
+//    List<DonationList> findDonationSummaries();
+//    @Query("SELECT new com.chocobean.donation.dto.DonationList(d.donationTitle, d.donationOrganization, "
+//            + "CAST((d.donationCurrentAmount * 100.0 / d.donationGoalAmount) AS int), "
+//            + "d.donationCurrentAmount, d.donationImg, d.donationDeadlineDate) "
+//            + "FROM Donation d JOIN d.donationCode dc "
+//            + "WHERE dc = :code")
+//    List<DonationList> findDonationSummariesByDonationCode(@Param("code") Integer code);
+//
+//
+//    @Query("SELECT new com.chocobean.donation.dto.DonationList(d.donationTitle, d.donationOrganization, " +
+//            "CAST((d.donationCurrentAmount * 100.0 / d.donationGoalAmount) AS int), " +
+//            "d.donationCurrentAmount, d.donationImg, d.donationDeadlineDate) " +
+//            "FROM Donation d ORDER BY d.donationDeadlineDate ASC")
+//    List<DonationList> findAllByOrderByDonationDeadlineDateAsc();
+//
+//    @Query("SELECT new com.chocobean.donation.dto.DonationView(" +
+//            "d.donationTitle, d.donationContent, d.donationInstitutionCode, d.donationOrganization, " +
+//            "d.donationTarget, d.donationTargetPeople, d.donationGoalAmount, d.donationCurrentAmount, " +
+//            "d.donationAmountPlan, d.donationCreateDate, d.donationDeadlineDate, d.donationImg" +
+//            ") " +
+//            "FROM Donation d WHERE d.donationNo = :donationNo")
+//    DonationView findDonationDetailsByDonationNo(@Param("donationNo") Long donationNo);
 
+    List<Donation> findByDonationCode(Integer code); // Query Method 방식
 
-    @Query("SELECT new com.chocobean.donation.dto.DonationList(d.donationTitle, d.donationOrganization, "
-            + "CAST((d.donationCurrentAmount * 100.0 / d.donationGoalAmount) AS int), "
-            + "d.donationCurrentAmount, d.donationImg, d.donationDeadlineDate) "
-            + "FROM Donation d JOIN d.donationCode dc "
-            + "WHERE dc = :code")
-    List<DonationList> findDonationSummariesByDonationCode(@Param("code") Integer code);
-
-    @Query("SELECT new com.chocobean.donation.dto.DonationList(d.donationTitle, d.donationOrganization, " +
-            "CAST((d.donationCurrentAmount * 100.0 / d.donationGoalAmount) AS int), " +
-            "d.donationCurrentAmount, d.donationImg, d.donationDeadlineDate) " +
-            "FROM Donation d ORDER BY d.donationDeadlineDate ASC")
-    List<DonationList> findAllByOrderByDonationDeadlineDateAsc();
-
-    @Query("SELECT new com.chocobean.donation.dto.DonationView(" +
-            "d.donationTitle, d.donationContent, d.donationInstitutionCode, d.donationOrganization, " +
-            "d.donationTarget, d.donationTargetPeople, d.donationGoalAmount, d.donationCurrentAmount, " +
-            "d.donationAmountPlan, d.donationCreateDate, d.donationDeadlineDate, d.donationImg" +
-            ") " +
-            "FROM Donation d WHERE d.donationNo = :donationNo")
-    DonationView findDonationDetailsByDonationNo(@Param("donationNo") Long donationNo);
+    List<Donation> findAllByOrderByDonationDeadlineDateAsc();
 }
