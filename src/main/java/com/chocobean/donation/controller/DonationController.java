bd2224a0f60ec89fdcec2f1be45fdf2a10dc718f
DESKTOP-2PUEH4H\kmcsl <kmcslo@naver.com>
2025-11-28 23:31:55 +0900

    no message

--- src/main/java/com/chocobean/donation/controller/DonationController.java
+++ src/main/java/com/chocobean/donation/controller/DonationController.java
@@ -3,8 +3,11 @@ package com.chocobean.donation.controller;
 import com.chocobean.donation.dto.DonationList;
 import com.chocobean.donation.dto.DonationView;
 import com.chocobean.donation.service.DonationService;
+import com.chocobean.donation.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +18,11 @@ import java.util.List;
 public class DonationController {
 
     private final DonationService donationService;
+    private final UserService userService;
 
     @GetMapping("/public/donations")
     public List<DonationList> getDonations(@RequestParam(defaultValue = "0") Integer categoryId) {
-        System.out.println(categoryId);
+
         return donationService.getDonations(categoryId);
     }
     @GetMapping("/public/donationsDate")
@@ -34,4 +38,21 @@ public class DonationController {
 
         return ResponseEntity.ok(donationData);
     }
+
+    @GetMapping("/public/donationApply")
+    public ResponseEntity<?> getDonationApply(
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        String userName = userDetails.getUsername();
+        int role = userService.getRoleByUserName(userName);
+        System.out.println("userName :"+ userName);
+        System.out.println("role :"+ role);
+        if (role == 1){
+            return ResponseEntity.status(403).body("NO_PERMISSION");
+        }else{
+            return ResponseEntity.ok("ok");
+        }
+    }
+
+
 }
