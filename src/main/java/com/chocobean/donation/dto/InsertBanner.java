5c1237bf0eff930d426797ec584c051eb99b5f41
DESKTOP-2PUEH4H\kmcsl <kmcslo@naver.com>
2026-02-03 21:08:27 +0900

    배너 추가

--- src/main/java/com/chocobean/donation/dto/InsertBanner.java
+++ src/main/java/com/chocobean/donation/dto/InsertBanner.java
@@ -1,5 +1,6 @@
 package com.chocobean.donation.dto;
 
+import com.chocobean.donation.entity.Banner;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,4 +24,15 @@ public class InsertBanner {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate bannerDeadlineDate;
+
+
+    public InsertBanner(Banner banner) {
+        this.bannerNo = banner.getBannerNo();
+        this.bannerTitle = banner.getBannerTitle();
+        this.bannerURL = banner.getBannerURL();
+        this.bannerImg = banner.getBannerImg();
+        this.bannerCreateDate = banner.getBannerCreateDate();
+        this.bannerStartDate = banner.getBannerStartDate();
+        this.bannerDeadlineDate = banner.getBannerDeadlineDate();
+    }
 }
